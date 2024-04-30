ee277b6f00a42b64104534e7d80630f98b561e77
Terry Moles <terry4715@gmail.com>
2024-04-30 08:53:18 +0100

    First kata complete

--- kata_1.py
+++ kata_1.py
@@ -2,4 +2,12 @@
 print("Hello, World!")
 # %%
 
-#kata #1 
+#kata #1 - https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+def make_negative(number):
+    return 0 - abs(int(number))
+
+number = input("Enter any number to make it negative: ")
+print(make_negative(number))
+
+#kata #2 - 
