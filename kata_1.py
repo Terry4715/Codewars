16d71dcb1a15978fada782cf5966a0acb42e6b20
Terry4715 <terry4715@gmail.com>
2024-05-06 19:04:33 +0100

    Kata #2

--- kata_1.py
+++ kata_1.py
@@ -7,7 +7,13 @@ print("Hello, World!")
 def make_negative(number):
     return 0 - abs(int(number))
 
-number = input("Enter any number to make it negative: ")
-print(make_negative(number))
+# number = input("Enter any number to make it negative: ")
+# print(make_negative(number))
 
-#kata #2 - 
+#kata #2 - https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+boolean = True
+print(bool_to_word(boolean))
